e9726252d04d80f3c6390794966a6efa5a2d12c3
FearGod12 <onyenikechukwudi@gmail.com>
2023-07-25 21:07:49 +0100

    task 3 fix

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -14,24 +14,20 @@ class LRUCache(BaseCaching):
         """puts an item into the cache"""
         if key is not None and item is not None:
             self.cache_data[key] = item
+
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                to_delete = self.keep_track.pop(0)
+                del self.cache_data[to_delete]
+                print("DISCARD: {}".format(to_delete))
             if key in self.keep_track:
                 self.keep_track.remove(key)
             self.keep_track.append(key)
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                to_delete = self.keep_track[0]
-                print(self.keep_track)
-                print("DISCARD: {}".format(to_delete))
-                print(self.cache_data)
-                del self.cache_data[to_delete]
-                self.keep_track.pop(0)
 
     def get(self, key):
-        """returns item of key in self.cache_data"""
+        """returns the value associated with a key"""
         if key is None:
             return None
-        if key in self.cache_data:
-            # Remove the key to update its access order
+        if key in self.keep_track:
             self.keep_track.remove(key)
-            self.keep_track.append(key)  # Add the key to the end of the list
-            return self.cache_data[key]
-        return None
+            self.keep_track.append(key)
+        return self.cache_data.get(key, None)
