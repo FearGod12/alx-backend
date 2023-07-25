3d3d6fe164c9e39cb852f0a9de312a8768b680fb
FearGod12 <onyenikechukwudi@gmail.com>
2023-07-25 20:59:38 +0100

    task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""MRUCache module"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """most recently used caching class"""
+
+    def __init__(self):
+        """class constructor"""
+        super().__init__()
+        self.keep_track = []
+
+    def put(self, key, item):
+        """puts into the cache"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                to_delete = self.keep_track.pop(-1)
+                del self.cache_data[to_delete]
+                print("DISCARD: {}".format(to_delete))
+            if key in self.keep_track:
+                self.keep_track.remove(key)
+            self.keep_track.append(key)
+
+    def get(self, key):
+        """returns the value associated with a key"""
+        if key is None:
+            return None
+        if key in self.keep_track:
+            self.keep_track.remove(key)
+            self.keep_track.append(key)
+        return self.cache_data.get(key, None)
