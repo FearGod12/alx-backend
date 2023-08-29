a91dab6580d642ce8a2645bc4432d98e28e51966
FearGod12 <onyenikechukwudi@gmail.com>
2023-08-29 18:55:38 +0100

    task 2

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,27 @@
+import { createClient, print } from "redis";
+
+const client = createClient()
+client.on('connect', () => {
+    console.log('Redis client connected to the server')
+});
+
+client.on('Error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`);
+})
+
+
+function setNewSchool(schoolName, value) {
+    const reply = client.SET(schoolName, value, (err, value) => {
+        print(`Reply: ${value}`);
+    })
+}
+
+function displaySchoolValue(schoolName) {
+    const reply = client.GET(schoolName, (err, reply) => {
+        console.log(reply);
+    })
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
