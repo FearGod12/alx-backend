9640301515ae3f75376451b2c0ddee5e1c433a41
FearGod12 <onyenikechukwudi@gmail.com>
2023-08-29 21:57:52 +0100

    task 4

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,41 @@
+import { createClient, print } from "redis";
+import { promisify } from 'util';
+
+const client = createClient()
+client.on('connect', () => {
+    console.log('Redis client connected to the server')
+});
+
+client.on('Error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`);
+})
+const items = {
+    'Portland': 50,
+    'Seattle': 80,
+    'New York': 20,
+    'Bogota': 20,
+    'Cali': 40,
+    'Paris': 2,
+    }
+    for (let item of Object.entries(items)) {
+        client.hset('HolbertonSchools', item[0], item[1], (err, reply) => {
+            if (err) print(err);
+            else {
+                print(reply);
+            }
+        });
+    }
+
+// for (let item of Object.entries(items)) {
+//     client.hdel('HolbertonSchools', item[0], (err, reply) =>
+//     {
+//         print(reply);
+//         })
+//     }
+
+client.hgetall('HolbertonSchools', (err, hashobject) => {
+    if (err) console.log(err);
+    else {
+        console.log(hashobject);
+    }
+})
